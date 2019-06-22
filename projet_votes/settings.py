fb91535f36ca94134a8291bc37abd0528926923f
lincal2309 <christophe.gressin@tofographies.com>
2019-06-22 06:28:10 +0200

    Minor updates

--- projet_votes/settings.py
+++ projet_votes/settings.py
@@ -21,14 +21,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a1nvi(1u2(*e12at&b9%n$qgkolly2k#$*=l0%=hw-8+k88!w@'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-if not DEBUG:
+
+if DEBUG:
+    SECRET_KEY = 'a1nvi(1u2(*e12at&b9%n$qgkolly2k#$*=l0%=hw-8+k88!w@'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    # EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+else:
     ALLOWED_HOSTS = ['127.0.0.1']
+    SECRET_KEY = os.environ['SECRET_KEY']
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 # Application definition
@@ -130,11 +135,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGIN_URL = '/polls/login/'
-
-if DEBUG:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    # EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
-    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 # List of colors used in charts
