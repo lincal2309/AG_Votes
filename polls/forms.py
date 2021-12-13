c1a1081b2738f23f1e5699c992379822c5e9f4b5
lincal2309 <christophe.gressin@tofographies.com>
2021-12-13 06:31:41 +0100

    Minor changes

--- polls/forms.py
+++ polls/forms.py
@@ -18,8 +18,8 @@ from .models import (
 class CreateUserForm(forms.Form):
     username = forms.CharField(label="Nom d'utilisateur", max_length=30)
     password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
-    # company=forms.ModelChoiceField(label="Société",
-    #     queryset=Company.objects.all())
+    company=forms.ModelChoiceField(label="Société",
+        queryset=Company.objects.all())
 
 
 # Login view
